fbeb054eba70e20a380d43d4adac3ebb7460dda2
Pedro Dias Vicente (Gleentech Solutions) <pedrolvicente@gmail.com>
2025-08-05 13:59:11 +0100

    adding Linux

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_KayteIDE_FILE /Users/pedro/Projects/kayteide/build/bin/KayteIDE.app/Contents/MacOS/KayteIDE)
-set(__QT_DEPLOY_TARGET_KayteIDE_TYPE EXECUTABLE)
--- build/CMakeFiles/KayteIDE_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for KayteIDE_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
